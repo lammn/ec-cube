314f50da1af1ba56e8212317cf76846fa429f6aa
hoand <ndhoa@lockon-vn.com>
2016-07-18 11:59:43 +0700

    - Define abstract class for changing config file ( include : htaccess + webconfig )

---
+++ src/Eccube/Service/AbstractWebServerConfigService.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Eccube\Service;
+
+
+abstract class AbstractWebServerConfigService
+{
+    abstract protected function readConfig($source);
+
+    abstract protected function getVersion();
+
+    abstract protected function getRewriteTextConfig($version);
+
+    abstract protected function parseConfig($source);
+
+    abstract protected function appendConfig($config, $append);
+
+    abstract protected function writeConfig($content, $destination);
+
+    public function backupConfig($source, $destination)
+    {
+        $configContent = $this->readConfig($source);
+        $this->writeConfig($configContent, $destination);
+    }
+
+    public function changeConfig($source)
+    {
+        $version = $this->getVersion();
+        $append = $this->getRewriteTextConfig($version);
+        $content = $this->parseConfig($source);
+        $configContent = $this->appendConfig($content, $append);
+        $this->writeConfig($configContent, $source);
+    }
+}
+
